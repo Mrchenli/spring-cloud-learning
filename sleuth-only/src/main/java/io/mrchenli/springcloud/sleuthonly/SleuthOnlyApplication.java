302130f29fd0a7b269442ba9602f271a4e9fe6a2
chenli <chenli@youyuwo.com>
2018-10-26 10:12:50 +0800

    add config server dev yml

---
+++ sleuth-only/src/main/java/io/mrchenli/springcloud/sleuthonly/SleuthOnlyApplication.java
@@ -0,0 +1,12 @@
+package io.mrchenli.springcloud.sleuthonly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthOnlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleuthOnlyApplication.class, args);
+    }
+}
